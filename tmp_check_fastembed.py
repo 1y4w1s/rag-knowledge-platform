4c8555abecbd6e5773395fd3cab0d4ad2e1d8f1e
1y4w1s <1287282101@qq.com>
2026-07-20 16:15:34 +0800

    docs: 清理7个过期文档+2个临时脚本

--- tmp_check_fastembed.py
+++
@@ -1,5 +0,0 @@
-from fastembed import TextEmbedding
-models = TextEmbedding.list_supported_models()
-bge = [m for m in models if 'bge' in m['model'].lower()]
-for m in bge:
-    print(f"{m['model']}: dim={m['dim']}")
